08a1c77e6841ec62474d31d314337dbd594cb9db
Mahmoud Elshennawy <shennawy25@gmail.com>
2019-08-18 18:16:49 +0200

    Created Pivot table for m:m relationships

---
+++ database/migrations/2019_08_18_161503_category_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CategoryProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Created using command:
+        //php artisan make:migration category_product_table --create=category_product
+        //Must name models, order must be alphabetical
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_product');
+    }
+}
