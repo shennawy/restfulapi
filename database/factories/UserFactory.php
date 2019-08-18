a77ec99c092e430634282ef4a1a1295e9bd53756
Mahmoud Elshennawy <shennawy25@gmail.com>
2019-08-18 18:38:11 +0200

    Implemented User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,11 +16,17 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => Str::random(10),
+        'verified' => $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]),
+        'verification_token' => $verified == User::VERIFIED_USER ? null: User::generateVerificationCode(),
+        'admin' => $admin = $faker->randomElement([User::ADMIN_USER, User::REGULAR_USER]),
+        
     ];
 });
