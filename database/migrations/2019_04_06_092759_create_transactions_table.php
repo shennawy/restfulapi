e67e872d051258d6581b6b04d2805615ff03499d
Mahmoud Elshennawy <shennawy25@gmail.com>
2019-08-18 18:11:54 +0200

    Implemented Migration for Transactions, foreign keys to users, products

--- database/migrations/2019_04_06_092759_create_transactions_table.php
+++ database/migrations/2019_04_06_092759_create_transactions_table.php
@@ -15,7 +15,13 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('quantity');
+            $table->integer('buyer_id');
+            $table->integer('product_id');
             $table->timestamps();
+
+            $table->foreign('buyer_id')->references('id')->on('users');
+            $table->foreign('product_id')->references('id')->on('products');
         });
     }
 
