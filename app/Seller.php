e5dcd60263052ad5dccea832025816561d5f0c9c
Mahmoud Elshennawy <shennawy25@gmail.com>
2019-08-18 18:57:48 +0200

    Fixed issue in Seller, added hasMany relationship to Product

--- app/Seller.php
+++ app/Seller.php
@@ -2,10 +2,15 @@
 
 namespace App;
 
+use App\Product;
 //Not using Model anymore, below line not needed
 //use Illuminate\Database\Eloquent\Model;
 
 class Seller extends User
 {
-    //
+    //Seller has many products
+    public function products()
+    {
+        return $this->hasMany(Product::class);    
+    }
 }
